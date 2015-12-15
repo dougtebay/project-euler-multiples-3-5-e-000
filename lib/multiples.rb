a1bcaf9d209915e71e7a791bbd01c7feefe430d3
dougtebay <dougtebay@gmail.com>
2015-12-15 16:57:26 -0500

    All tests passing

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+def collect_multiples(num)
+  arr = Array.new
+  (1...num).each do |i|
+    i % 3 == 0 || i % 5 == 0 ? arr << i : i
+  end
+  arr
+end
+
+def sum_multiples(num)
+  sum = 0
+  (1...num).each do |i|
+    i % 3 == 0 || i % 5 == 0 ? sum += i : i
+  end
+  sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,22 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(num)
+    @num = num
+  end
+
+  def collect_multiples
+    arr = Array.new
+    (1...@num).each do |i|
+      i % 3 == 0 || i % 5 == 0 ? arr << i : i
+    end
+    arr
+  end
+
+  def sum_multiples
+    sum = 0
+    (1...@num).each do |i|
+      i % 3 == 0 || i % 5 == 0 ? sum += i : i
+    end
+    sum
+  end
+end
